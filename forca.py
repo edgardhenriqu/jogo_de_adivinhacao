def36af15946d125dddfb2a2bbead0d8cb6f5c26
Edgard <edigarhenriqu@hmail.com>
2023-04-18 13:22:28 -0300

    Manipulando String

--- forca.py
+++ forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("Bem vindo no jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquato (True)
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando...")
+
+
     print("Fim de jogo")
 
 if(__name__ == "__main__"):
