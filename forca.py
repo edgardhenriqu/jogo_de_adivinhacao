31e4ce939228f8200df222b69af7ccd44df3e452
Edgard <edigarhenriqu@hmail.com>
2023-04-22 14:24:45 -0300

    List Comprehension

--- forca.py
+++ forca.py
@@ -3,13 +3,12 @@ def jogar():
     print("Bem vindo no jogo de Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-    letras_faltando = str(letras_acertadas.count('_'))
-    print( 'ainda falta acerta {} letras'.format(letras_faltando))
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
@@ -17,17 +16,27 @@ def jogar():
     while(not enforcou and not acertou):
 
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
 
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu")
     print("Fim de jogo")
 
 if(__name__ == "__main__"):
